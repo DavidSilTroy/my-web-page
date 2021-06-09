a63d962cd7b099f8ddeae0ad52042658a0bc277d
Davidsiltroy <davidsiltroy2008@hotmail.com>
2021-06-08 21:23:38 -0500

    Improving Show Experience

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -18,6 +18,8 @@ window.onscroll = () => {
     console.log(davst.screenY)
     colorMenu();
     showProjects();
+    showSkills();
+    showExperience();
     if (!davst.menuHide) {
         menuHideShow();
     }
@@ -48,7 +50,6 @@ const menuHideShow = () => {
         colorMenu();
     }
 };
-
 const colorMenu = (color) => {
     if (color == null) {
         if (davst.screenY > 20 && davst.menuHide) {
@@ -80,61 +81,126 @@ const colorMenu = (color) => {
 }
 const showProjects = () => {
     var px1 = 0;
-    var px2 = 400;
-    var px3 = 800;
-    /* if (screen.width < 994 && screen.width >= 425) {
-        px1 = 0;
-        px2 = 440;
-        px3 = 1100;
-    } */
-    if (screen.width >= 994) {
-        px1 = 0;
-        px2 = 0;
-        px3 = 0;
+    var px2 = 0;
+    var px3 = 0;
+    var delay_1 = 0.3;
+    var delay_2 = 0.6;
+    var delay_3 = 0.9;
+    if (screen.width < 994) {
+        px2 = 300;
+        px3 = 600;
+        delay_1 = 0.3;
+        delay_2 = 0;
+        delay_3 = 0;
     }
 
     if (davst.screenY >= px1) {
+
         if (davst.showedProject < 1) {
             gsap.from("#davst_project-1", {
-                x: 100,
+                y: 100,
                 duration: 1,
+                delay: delay_1
             });
             gsap.to("#davst_project-1", {
-                opacity: 1
+                opacity: 1,
+                duration: 1,
+                delay: delay_1
             });
             davst.showedProject++;
         }
     }
-    if (davst.screenY > px2) {
+    if (davst.screenY >= px2) {
         if (davst.showedProject < 2) {
             gsap.from("#davst_project-2", {
-                x: 100,
+                y: 100,
                 duration: 1,
+                delay: delay_2
             });
             gsap.to("#davst_project-2", {
-                opacity: 1
+                opacity: 1,
+                duration: 1,
+                delay: delay_2
             });
             davst.showedProject++;
         }
     }
-    if (davst.screenY > px3) {
+    if (davst.screenY >= px3) {
         if (davst.showedProject < 3) {
             gsap.from("#davst_project-3", {
                 x: 100,
                 duration: 1,
+                delay: delay_3
             });
             gsap.to("#davst_project-3", {
-                opacity: 1
+                opacity: 1,
+                duration: 1,
+                delay: delay_3
             });
             davst.showedProject++;
         }
-        //Aqui para los skillss
 
     }
 }
+const showExperience = () => {
+    var delay_1 = 0.3;
+    var px1 = 570;
+    if (davst.screenY >= px1) {
+        if (davst.showedProject < 5) {
+            gsap.from("#exp-youtube", {
+                x: -100,
+                duration: 1,
+                delay: delay_1
+            });
+            gsap.to("#exp-youtube", {
+                opacity: 1,
+                duration: 1,
+                delay: delay_1
+            });
+            gsap.from("#exp-github", {
+                y: 100,
+                duration: 1,
+                delay: delay_1
+            });
+            gsap.to("#exp-github", {
+                opacity: 1,
+                duration: 1,
+                delay: delay_1
+            });
+            gsap.from("#exp-instagram", {
+                x: 100,
+                duration: 1,
+                delay: delay_1
+            });
+            gsap.to("#exp-instagram", {
+                opacity: 1,
+                duration: 1,
+                delay: delay_1
+            });
+            davst.showedProject++;
+        }
+    }
+}
+const showSkills = () => {
+    var delay_1 = 0.3;
+    var px1 = 830;
+    if (davst.screenY >= px1) {
+        if (davst.showedProject < 4) {
+            gsap.from("#skills-showed", {
+                y: 100,
+                duration: 1,
+                delay: delay_1
+            });
+            gsap.to("#skills-showed", {
+                opacity: 1,
+                duration: 1,
+                delay: delay_1
+            });
+            davst.showedProject++;
+        }
+    }
+}
 const startWith = () => {
-
-    showProjects();
     var t1 = gsap.timeline();
     t1.from("#davst_first-title", {
             y: -50,
@@ -156,6 +222,14 @@ const startWith = () => {
             duration: 1,
             opacity: 0,
         }, "-=0.5");
+    if (screen.width >= 994) {
+        for (let i = 0; i < 3; i++) {
+            showProjects();
+            console.log(davst.showedProject);
+        }
+    } else {
+        showProjects();
+    }
 }
 
 
